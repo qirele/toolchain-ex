a0127537c4840bb07902978fbbee6ea02df0e6ee
qirele <kirilczikzik@gmail.com>
2022-02-21 20:06:39 +0100

    please bro

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> DUDE I HATE GIT CONFIG
+        <strong>{results.length}</strong> dude this is insane
       </p>
       <hr></hr>
       {results
