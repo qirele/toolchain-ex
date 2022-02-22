a5e5bb2423820a924df9c46d92621017bf6a1062
qirele <kirilczikzik@gmail.com>
2022-02-22 11:35:25 +0100

    commit no.5 from vscode

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> COMMIT NO.4 BRUH BRUH BRHU
+        <strong>{results.length}</strong> COMMIT NO.5 from vscode
       </p>
       <hr></hr>
       {results
